2a95bf92aa3f78220a0ed17150b858a42563a44d
divija027 <120162688+divija027@users.noreply.github.com>
2024-11-15 10:44:48 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -22,186 +22,225 @@ res = sender()
 receiver(res)
 `,
 
-    button2: `Button b1, b2, b3, b4;
-TextView res;
-EditText n1, n2;
+    button2: `
+def bit_stuffing(data):
+    stuffed_data = ""
+    count = 0
+    for bit in data:
+        if bit == '1':
+            count += 1
+            stuffed_data += bit
+        else:
+            count = 0
+            stuffed_data += bit
+        if count == 5:
+            stuffed_data += '0'  
+            count = 0
+    return stuffed_data
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-    n1 = findViewById(R.id.num1);
-    n2 = findViewById(R.id.num2);
-    b1 = findViewById(R.id.add);
-    b2 = findViewById(R.id.sub);
-    b3 = findViewById(R.id.mul);
-    b4 = findViewById(R.id.div);
-    res = findViewById(R.id.t1);
+# Bit decoding function
+def bit_decoding(stuffed_data):
+    decoded_data = ""
+    count = 0
+    for bit in stuffed_data:
+        if bit == '1':
+            count += 1
+            decoded_data += bit
+        else:
+            if count == 5:
+                
+                count = 0
+                continue
+            count = 0
+            decoded_data += bit
+    return decoded_data
 
-    b1.setOnClickListener(view -> {
-        int a1 = Integer.parseInt(n1.getText().toString());
-        int a2 = Integer.parseInt(n2.getText().toString());
-        int result = a1 + a2;
-        res.setText("Result: " + result);
-    });
+data = input("Enter the data (binary string): ")
+stuffed_data = bit_stuffing(data)
+print("Data after bit stuffing:", stuffed_data)
+decoded_data = bit_decoding(stuffed_data)
+print("Data after bit decoding:", decoded_data)
+
+`,
+
+    button3: `def div(num1, num2):
+    n = len(num2)
+    result = num1[:n]
+    rem = ""
     
-    b2.setOnClickListener(view -> {
-        int a1 = Integer.parseInt(n1.getText().toString());
-        int a2 = Integer.parseInt(n2.getText().toString());
-        int result = a1 - a2;
-        res.setText("Result: " + result);
-    });
     
-    b3.setOnClickListener(view -> {
-        int a1 = Integer.parseInt(n1.getText().toString());
-        int a2 = Integer.parseInt(n2.getText().toString());
-        int result = a1 * a2;
-        res.setText("Result: " + result);
-    });
+    for i in range(len(num2)):
+        if result[i] == num2[i]:
+            rem += "0"
+        else:
+            rem += "1"
+
+    while n < len(num1):
+        if rem[0] == '0':
+            rem = rem[1:] + num1[n]
+        else:
+            rem = rem[1:] + num1[n]
+            rem = div(rem, num2)
+        n += 1
     
-    b4.setOnClickListener(view -> {
-        int a1 = Integer.parseInt(n1.getText().toString());
-        int a2 = Integer.parseInt(n2.getText().toString());
-        if (a2 == 0) {
-            res.setText("Cannot divide by 0");
-        } else {
-            int result = a1 / a2;
-            res.setText("Result: " + result);
-        }
-    });
+    result = rem
+    rem = ""
+    for i in range(len(num2)):
+        if result[i] == num2[i]:
+            rem += "0"
+        else:
+            rem += "1"
+    return rem[1:]
+
+def main():
+    gen = input("Enter Generator: ")
+    data = input("Enter Data: ")
+    code = data
+
+    
+    while len(code) < (len(data) + len(gen) - 1):
+        code += "0"
+    
+    
+    code = data + div(code, gen)
+    print("Transmitted code word:", code)
+    
+    
+    rec = input("Please Enter the received code: ")
+    if int(div(rec, gen)) == 0:
+        print("The received code word contains no errors")
+    else:
+        print("The received code word contains errors")
+
+if __name__ == "__main__":
+    main()
+
+`,
+
+    button4: `def distance_vector_routing():
+    
+    n = int(input("Enter the number of nodes: "))
+    
+    
+    dmat = []
+    print("Enter the cost matrix:")
+    for i in range(n):
+        row = list(map(int, input(f"Row {i+1}: ").split()))
+        row[i] = 0  # Set distance to self as 0
+        dmat.append(row)
+    
+    
+    dist = [[dmat[i][j] for j in range(n)] for i in range(n)]
+    via = [[j for j in range(n)] for i in range(n)]
+    
+    
+    updated = True
+    while updated:
+        updated = False
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    # If a shorter path is found, update the distance and path
+                    if dist[i][j] > dmat[i][k] + dist[k][j]:
+                        dist[i][j] = dmat[i][k] + dist[k][j]
+                        via[i][j] = k
+                        updated = True
+    
+    
+    for i in range(n):
+        print(f"\nRouting table for node {i + 1}:")
+        for j in range(n):
+            print(f" To node {j + 1} via {via[i][j] + 1} with distance {dist[i][j]}")
+
+
+if __name__ == "__main__":
+    distance_vector_routing()
+
+`,
+
+    button5: `
+import java.util.*;
+public class leakybucket {
+public static void flow(int pktsize , int output){
+int bucketsize=512;
+if(pktsize>bucketsize) {
+System.out.println("Bucket overflow");
+}else {
+try {
+Thread.sleep(10);
+while(pktsize>output) {
+System.out.println(output+"bytes outflow ");
+pktsize=pktsize-output;
+}
+if(pktsize>0) {
+System.out.println(pktsize+"bytes outflow");
+}
+}catch(InterruptedException e) {
+System.out.println("Interrupted");
+}
+}
+}
+public static void main(String[]args) {
+int output,pktsize,n;
+Scanner input = new Scanner (System.in);
+Random rand = new Random();
+System.out.println("Enter output rate ");
+output=input.nextInt();
+System.out.println("Enter the number of packets");
+n=input.nextInt();
+for(int i=1;i<=n;i++) {
+pktsize=rand.nextInt(1000);
+System.out.println("packet no:"+i+"packet size="+pktsize);
+flow(pktsize,output);
+}
+}
 }
 `,
 
-    button3: `RadioGroup r;
-ConstraintLayout ly;
+    button6: `import socket
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("socket created!!!")
+s.connect(("localhost",9999))
+data=input("Enter ur name: ")
+s.send(bytes(data,"utf-8"))
+print("Data sent!!!")
+response=s.recv(1024).decode()
+print(response)
+s.close()
+#reciever
+import socket
+r=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("socket created!!!")
+r.bind(("localhost",9999))
+r.listen(3)
+print("waiting for client!!!")
+c,addr=r.accept()
+data=c.recv(1024).decode()
+print(f"received data: {data}")
+c.send(bytes("data received by server!!!","utf-8"))
+c.close()
+r.close(
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-    r = findViewById(R.id.rg);
-    ly = findViewById(R.id.l);
-    r.setOnCheckedChangeListener((radioGroup, i) -> {
-        if (i == R.id.red) {
-            ly.setBackgroundColor(Color.RED);
-        } else if (i == R.id.green) {
-            ly.setBackgroundColor(Color.GREEN);
-        } else {
-            ly.setBackgroundColor(Color.BLUE);
-        }
-    });
-}
 `,
 
-    button4: `TextView t;
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-}
-
-@Override
-public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.optionsmenu, menu);
-    return super.onCreateOptionsMenu(menu);
-}
-
-@Override
-public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-    t = findViewById(R.id.tv);
-    int id = item.getItemId();
-    if (id == R.id.settings) {
-        t.setText("Settings selected");
-    } else if (id == R.id.history) {
-        t.setText("History selected");
-    } else {
-        t.setText("About selected");
-    }
-    return super.onOptionsItemSelected(item);
-}
-`,
-
-    button5: `ImageView i;
-Button b;
-int[] images = {R.drawable.elephant, R.drawable.moon, R.drawable.rose};
-int currentIndex = 0;
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-    i = findViewById(R.id.image);
-    b = findViewById(R.id.cb);
-    b.setOnClickListener(view -> {
-        currentIndex = (currentIndex >= images.length) ? 0 : currentIndex;
-        i.setImageResource(images[currentIndex]);
-        currentIndex++;
-    });
-}
-`,
-
-    button6: `Log.i("ActivityLc", "onCreate() method");
-
-@Override
-protected void onStart() {
-    super.onStart();
-    Log.i("ActivityLc", "onStart() method");
-}
-
-@Override
-protected void onResume() {
-    super.onResume();
-    Log.i("ActivityLc", "onResume() method");
-}
-
-@Override
-protected void onPause() {
-    super.onPause();
-    Log.i("ActivityLc", "onPause() method");
-}
-
-@Override
-protected void onStop() {
-    super.onStop();
-    Log.i("ActivityLc", "onStop() method");
-}
-
-@Override
-protected void onRestart() {
-    super.onRestart();
-    Log.i("ActivityLc", "onRestart() method");
-}
-
-@Override
-protected void onDestroy() {
-    super.onDestroy();
-    Log.i("ActivityLc", "onDestroy() method");
-}
-`,
-
-    button7: `Button b;
-ConstraintLayout ly;
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-    b = findViewById(R.id.rc);
-    ly = findViewById(R.id.l);
-    b.setOnClickListener(view -> {
-        Random rn = new Random();
-        int r = rn.nextInt(256);
-        int g = rn.nextInt(256);
-        int b = rn.nextInt(256);
-        ly.setBackgroundColor(Color.rgb(r, g, b));
-    });
-}
+    button7: `#sender
+import socket
+c=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+data=input("Enter the data: ")
+c.sendto(bytes(data,"utf-8"),("localhost",9999))
+print("Data sent to server!!!")
+response,addr=c.recvfrom(1024)
+print(response.decode())
+c.close()
+#reciever
+import socket
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+print("socket created!!!")
+s.bind(("localhost",9999))
+data,addr=s.recvfrom(1024)
+print(f"data received: {data.decode()}")
+s.sendto(bytes("Data received by server","utf-8"),addr)
+s.close()
 `,
 
     button8: `CheckBox c1, c2, c3, c4;
